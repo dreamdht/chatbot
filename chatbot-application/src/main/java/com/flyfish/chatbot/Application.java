c762644a6e9d95d9015c199b9650b8b3769357d6
flyfish <1850370711@qq.com>
2023-05-10 11:56:08 +0800

    v1.0:init project

---
+++ chatbot-application/src/main/java/com/flyfish/chatbot/Application.java
@@ -0,0 +1,16 @@
+package com.flyfish.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @autohr flyfish
+ * @date: 2023/5/10 11:43
+ * @description:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ chatbot-infrastructure/src/main/java/com/flyfish/chatbot/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @autohr flyfish
+ * @date: 2023/5/10 11:49
+ * @description:
+ */
+package com.flyfish.chatbot;
